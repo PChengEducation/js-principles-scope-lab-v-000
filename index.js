4c9eb43e98f4cc9fca83b2b2965348f55f72b345
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 05:14:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your solution in this file!
 var customerName = "bob";
-function upperCaseCustomerName(customerNmae){
+function upperCaseCustomerName(customerName){
   customerName = customerName.toUpperCase();
   return customerName;
 }
-function setBestCustomer(bestCustomer){
+function setBestCustomer(customerName){
   var bestCustomer = "not bob";
   return bestCustomer;
 }
