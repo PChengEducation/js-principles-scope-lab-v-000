7c900e6d075156baef703a82675f096076dac225
Pan Cheng <pchengeducation@hotmail.com>
2018-08-03 04:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution in this file!
 var customerName = "bob";
 function upperCaseCustomerName(customerNmae){
-  customerName = customerName.uppercases;
+  customerName = customerName.uppercase;
+  return customerName;
 }
